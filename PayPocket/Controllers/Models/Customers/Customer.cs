60790898bec2b817db53ab05eed79e68025f651a
Kingswithkings <kingsuthanaidogu@gmail.com>
2024-04-04 15:27:29 +0100

    Add project files.

---
+++ PayPocket/Controllers/Models/Customers/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace PayPocket.Controllers.Models.Customer
+{
+    public class Customer
+    {
+        public int Guid { get; set; } 
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }    
+        public string PhoneNumber { get; set; } 
+    }
+}

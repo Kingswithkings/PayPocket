711d8f66bceed7dbd63aea07e56088fbc9637938
Kingswithkings <kingsuthanaidogu@gmail.com>
2024-04-13 13:37:32 +0100

    program.cs usesql server

--- PayPocket/Controllers/Models/Customers/Customer.cs
+++ PayPocket/Controllers/Models/Customers/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public int Guid { get; set; } 
+        public Guid Id { get; set; } 
         public string FirstName { get; set; }   
         public string LastName { get; set; }    
         public string PhoneNumber { get; set; } 
